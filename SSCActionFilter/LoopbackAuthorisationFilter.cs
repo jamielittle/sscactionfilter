3ff84f0746680a3d66fcf89f2305233a9defe306
jamie.little <jamie.little@calonwebsolutions.co.uk>
2015-02-18 18:42:14 +0000

    First incarnation of LoopbackAuthorisationFilter

---
+++ SSCActionFilter/LoopbackAuthorisationFilter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+
+namespace SSCActionFilter
+{
+    public class LoopbackAuthorisationFilter : System.Web.Http.Filters.AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            // Play nicely with the base class :)
+            base.OnAuthorization(actionContext);
+
+            // If the request does not originate from this machine
+            if (!System.Web.HttpContext.Current.Request.IsLocal)
+            {
+                // Create an Unauthorised Access response
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized, "Unauthorised Access");
+            }
+        }
+    }
+}
